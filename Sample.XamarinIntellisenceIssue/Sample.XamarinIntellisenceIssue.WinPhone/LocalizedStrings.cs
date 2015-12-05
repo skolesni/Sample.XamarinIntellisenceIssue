1f358dd7477ae8112a6df71013fefb3a5ffd1442
Stan Kolesnik <stanislav_kolesnik@hotmail.com>
2015-12-05 11:12:35 +0000

    Create Solution

---
+++ Sample.XamarinIntellisenceIssue/Sample.XamarinIntellisenceIssue.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Sample.XamarinIntellisenceIssue.WinPhone.Resources;
+
+namespace Sample.XamarinIntellisenceIssue.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Created in Visual Studio Professional 2015, Version 14.0.24720.00 Update 1
+
+Step 1. Create Solution. File -> New -> Project -> Cross-Platform -> Blank App (Xamarin.Forms Portable); Name: Sample.XamarinIntellisenceIssue
